623c6b07e625dd0742357c96f921c644ee8de447
Shubham Bhatia <Shubhambhatia2103@gmail.com>
2024-03-01 18:40:55 +0530

    Data Type

---
+++ 03/script.js
@@ -0,0 +1,25 @@
+//  Data Types
+//  Numbers
+
+let num = 3.14;
+let num2 = 10;
+
+console.log(num + num2);
+
+console.log('------')
+//  Strings
+let firstName = '123shubh@m';    // using '--'
+const lastName = "Bhatia";      // using "--"
+
+console.log(firstName + ' ' + lastName);
+
+console.log('------')
+//  Booleans
+let isLoggedIn = true
+console.log(isLoggedIn)
+
+console.log('------')
+//  Null
+let lastLoginDate = null;
+console.log(lastLoginDate)
+
