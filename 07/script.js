b432b8a433ae9eef13cd7995ae86804c656bb583
Shubham Bhatia <Shubhambhatia2103@gmail.com>
2024-03-02 22:42:53 +0530

    Functions

--- 07/script.js
+++ 07/script.js
@@ -12,10 +12,44 @@ function add(a ,b) {
 }
 add(5, 5)   //  Parameters
 
-
+console.log('-------')
 function multi(a, b) {
     // console.log(a * b);
     return a * b;
 }
 let c = multi(2, 4)
 console.log('Hey user your result is', c)
+
+
+console.log('-------')
+//  Add multiple number
+//  Unlimited Arguments accept
+
+function addNumbers() {
+    let ans = 0
+    for (let i = 0; i < arguments.length; i++){
+        ans = ans + arguments[i]
+    }
+    return ans
+}
+
+let results = addNumbers(20, 30, 40, 50, 60)
+console.log(results)
+
+
+console.log('-------')
+// Spread Operator --> ...
+function addNumbersV2(...shubham) {
+    let ans = 0
+    for (let i = 0; i < shubham.length; i++){
+        ans = ans + shubham[i]
+    }
+    return ans
+}
+
+let result = addNumbers(200, 30, 40, 50, 60)
+console.log(result)
+
+
+
+
