2512f98b96aa1bc895b427223a973539aa86b9a2
Shubham Bhatia <Shubhambhatia2103@gmail.com>
2024-03-02 18:01:30 +0530

    Logical Operators

--- 05/script.js
+++ 05/script.js
@@ -1,1 +1,38 @@
-console.log('waddupp!')
+//  Logical Operators
+//  AND | OR | NOT
+
+//  AND --> (&&) all condition must be true
+
+const age = 20;
+const gender = 'female';
+ 
+if (age >= 18 && gender == 'male'){
+    console.log('good to go')
+}else {
+    console.log('nope')
+}
+
+
+console.log('--------')
+//  OR --> (||) Atleast one Cond. should be True
+const myage = 20;
+const mygender = 'female';
+
+if (myage >= 50 || mygender == 'female'){
+    console.log('good to go')
+}else {
+    console.log('nope')
+}
+
+
+console.log('--------')
+//  NOT --> (!) 
+//  True --> False
+//  False --> True
+
+const number = 4;
+if (number % 2 != 0){
+    console.log('Odd')
+}else {
+    console.log('Even')
+}
