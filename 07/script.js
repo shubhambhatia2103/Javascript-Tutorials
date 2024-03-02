06ae69f35052145c2b1d1cf4dd88ea6628587e04
Shubham Bhatia <Shubhambhatia2103@gmail.com>
2024-03-02 19:54:33 +0530

    Functions

--- 07/script.js
+++ 07/script.js
@@ -1,1 +1,21 @@
-console.log("Bonjor!")
+//  Functions
+//  A block of code that performs a particular task
+
+function sayHello() {
+    console.log('Hey Shubham')
+}
+sayHello()  //  Calling of Function
+
+//  Arguments
+function add(a ,b) {
+    console.log(a + b);
+}
+add(5, 5)   //  Parameters
+
+
+function multi(a, b) {
+    // console.log(a * b);
+    return a * b;
+}
+let c = multi(2, 4)
+console.log('Hey user your result is', c)
