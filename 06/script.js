4b846cb096865bb75c826c3c67b9f50bda415382
Shubham Bhatia <Shubhambhatia2103@gmail.com>
2024-03-02 18:34:33 +0530

    Loops

--- 06/script.js
+++ 06/script.js
@@ -1,1 +1,16 @@
-console.log("Lets learn Loops")
+//  Loops
+
+//  For --> If you know how many times 
+for(let i = 1; i <=10; i++){
+    console.log('Shubham' + i)
+}
+
+//  While  
+
+let ip = 25
+let house = 50
+
+while (ip != house){
+    ip = ip + 1;
+    console.log('Step Taken' + ip)
+}
