6d657d7fedc5a38d774461a10fde2615ba9baf66
Shubham Bhatia <Shubhambhatia2103@gmail.com>
2024-03-03 19:30:49 +0530

    High Order Functions and Callbacks

--- 09/script.js
+++ 09/script.js
@@ -1,1 +1,14 @@
-console.log("Namaste ji!")
+//  High Order Functions and Callbacks 
+//  A function that takes a function as an argument
+
+function add(a, b, cb){
+    let result = a + b;
+    cb(result);
+}
+
+// add(2, 4 , function (val) {
+//     console.log(val)
+// });
+
+add(2, 4, (val) => console.log(val));
+add(5, 5, (res) => console.log(res));
