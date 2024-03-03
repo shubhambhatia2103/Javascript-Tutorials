f30edb0e36f641e33d2dda3edf804a0ab5b1350e
Shubham Bhatia <Shubhambhatia2103@gmail.com>
2024-03-03 18:35:37 +0530

    Arrow Functions

--- 08/script.js
+++ 08/script.js
@@ -1,2 +1,28 @@
 //  Arrow Functions
 
+//  1. Syntax
+// function sayHello() {
+//     console.log('Hello!')
+// }
+// sayHello();
+
+const sayHello = () => {    //  Arrow Function
+    console.log('Hello!');
+}
+sayHello();
+
+// const add = (a, b) => {
+//     return a + b;
+// }
+// console.log(add(2, 3));
+
+const add = (a, b) => a + b;    //  One Liner 
+console.log(add(3, 3));
+
+//  2. 'arguments' keywords
+//  access multiple arguments
+
+const addNumbers = (...nums) => {
+    console.log(nums);
+}
+addNumbers(12, 13, 14, 15)
