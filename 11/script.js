4f6d54712a1cfe54237a855298ff77d375703870
Shubham Bhatia <Shubhambhatia2103@gmail.com>
2024-03-04 19:41:45 +0530

    Arrays High Order Functions

--- 11/script.js
+++ 11/script.js
@@ -1,4 +1,6 @@
 //  Arrays High Order Functions
+
+
 const students = ["Shubham", "John", "Doe"]
 console.log(students.length);   //  lenght of array
 console.log(students[0]);   //  Access particular array
@@ -21,8 +23,22 @@ const numbers = [1,2,3,4,5,6,7];
 
 //  foreach don't return the value
 //  map returns the value
-function double(n) {
-    return n * 2;
-}
-let newArr = numbers.map(double);
+
+// function double(n) {
+//     return n * 2;
+// }
+// let newArr = numbers.map(double);
+// console.log(newArr);
+
+// let ans = numbers.findIndex((num) => num == 5);
+// console.log(ans);
+
+// const newArr = numbers.filter((num) => num % 2 == 0)
+// console.log(newArr);
+
+// let newArr = numbers.slice(1, 5);
+// let newArr = numbers.slice(3);
+
+let newArr = numbers.splice(1, 4);
 console.log(newArr);
+console.log(numbers);
