c1db108eed84ef0c8eb2ea6c31ee439a7cfaea85
Shubham Bhatia <Shubhambhatia2103@gmail.com>
2024-03-04 18:35:16 +0530

    Arrays

--- 10/script.js
+++ 10/script.js
@@ -1,2 +1,15 @@
 //  Arrays
-console.log('Namaste Ji!')
+const students = ["Shubham", "John", "Doe"]
+console.log(students.length);   //  lenght of array
+console.log(students[0]);   //  Access particular array
+
+students[2] = 'Dosa';   //  change array element
+console.log(students[2]);
+
+students.push('Alexa');
+students.push('Ola');
+console.log(students);
+
+//  Heterogeneous
+const myArrays = [1, true, 'Shubham']
+console.log(myArrays)
